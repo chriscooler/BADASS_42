a9c9cfe571d484e1d60b8f1295332d798b6f239c
dagudelo <dagudelo@Debian.myguest.virtualbox.org>
2026-03-13 15:30:29 +0100

    Fixing P2 scripts

---
+++ P2/config_files/router_1_unicast/unicast.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ip link del vxlan10
+ip link add name vxlan10 type vxlan id 10 dev eth0 remote 10.1.1.2 local 10.1.1.1 dstport 4789
+ip link set vxlan10 up
+ip link set eth1 master br0
+ip link set vxlan10 master br0
+
